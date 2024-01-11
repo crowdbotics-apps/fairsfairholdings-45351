d7b12e67bd37bae3735a60ab8ab7e4e0a1b7e136
Crowdbotics <team@crowdbotics.com>
2024-01-11 02:34:34 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "fairsfairholdings-45351",
+  url: "https://fairsfairholdings-45351.botics.co",
+  api: "https://fairsfairholdings-45351.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
